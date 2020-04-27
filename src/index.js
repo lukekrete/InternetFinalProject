ae1470a4b604891312d50daac369436b2f17808e
lukekrete <lukekrete@gmail.com>
2020-04-26 20:31:48 -0400

    homepage commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './App'; //change this
+import Homepage from './HomepageLayout'
+import 'semantic-ui-css/semantic.min.css'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Homepage />  
   </React.StrictMode>,
   document.getElementById('root')
 );
